ad0a2c26bee7b42acda27c851e3e0cbae09b7d1b
BodyMass <main@bodymass.com>
2021-01-17 14:40:37 +0300

    sql script

---
+++ src/main/resources/script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  email varchar(200) NOT NULL,
+  password varchar(200) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id_UNIQUE (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ;
